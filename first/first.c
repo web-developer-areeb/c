bcf9de7fbfa7150b09ab1b77920ed2951d1f2bcd
Areeb Ahmad <58115189+web-developer-areeb@users.noreply.github.com>
2023-06-01 21:00:12 +0530

    Update first.c

--- first/first.c
+++ first/first.c
@@ -14,6 +14,6 @@ int main() {
 }
 
 // install gcc compiler one time
-// install c/c++ extenstion vs code one time
+// install c/c++ Intellisense Microsoft extenstion vs code one time
 // gcc -o haha first.c  to create executable file one time for new file
 // ./haha to execute  everytime
